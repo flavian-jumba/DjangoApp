fa9885624581f0bec2ed353e2ed83c342312dd59
flavian-jumba <flaviajumba@gmail.com>
2025-06-07 01:54:17 +0300

    Done with the serc filter, am moving on to authentication

--- myproject/baseapp/views.py
+++ myproject/baseapp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
-
+from django.db.models import Q  # Import Q for complex queries
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    roomsdata = Room.objects.filter(topic__name__icontains=q)
+    roomsdata = Room.objects.filter(
+        Q(topic__name__icontains=q) | 
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     topics = Topic.objects.all()
-    context = {'rooms': roomsdata, 'topics': topics}
+    room_count = roomsdata.count()
+    context = {'rooms': roomsdata, 'topics': topics, 'room_count': room_count}
     return render(request, 'baseapp/home.html', context)
 
 
