baf1da3213db46a2ea5ff028713704d350ce0564
flavian-jumba <flaviajumba@gmail.com>
2025-06-07 19:04:06 +0300

    made the message delete function

--- myproject/baseapp/views.py
+++ myproject/baseapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -78,13 +78,23 @@ def home(request):
 
 
 def room(request, pk):
-    try:
-        room = Room.objects.get(id=pk)
-        context = {'room': room}
-        return render(request, 'baseapp/room.html', context)
-    except Room.DoesNotExist:
-        messages.error(request, 'Room does not exist')
-        return redirect('home')
+    room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    
+
+    
+    if request.method == "POST":
+        message = Message.objects.create(    # Changed from message.objects to Message.objects
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)  # Add user to participants
+        return redirect('room', pk=room.id)
+    
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
+    return render(request, 'baseapp/room.html', context)
 
 @login_required(login_url='login')
 def createRoom(request):
@@ -121,3 +131,16 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'baseapp/delete.html', {'object': room})
+
+
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):    
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse("You are not allowed to delete this room.")
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request, 'baseapp/delete.html', {'object': message})
